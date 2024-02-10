fd6e90f9a24520f12044591e88248d31e1f1de2a
Shxuuer <a2711492759@163.com>
2024-02-10 16:58:18 +0800

    feat(userinfo): 稍微改了下样式

--- src/views/user/UserInfo.vue
+++ src/views/user/UserInfo.vue
@@ -12,6 +12,14 @@ export default {
   computed: {
     user () {
       return this.$store.getters.userInfo
+    },
+    userRole () {
+      const userRole = {
+        'super-admin': '超级管理员',
+        admin: '管理员',
+        customer: '游客'
+      }
+      return userRole[this.user.role]
     }
   },
   methods: {
@@ -38,18 +46,22 @@ export default {
 
 <template>
   <a-page-header style="border-bottom: 1px solid rgb(235, 237, 240);padding: 10px 20px;font-size: 1.1em" title="用户信息" @back="$router.push({name: 'Home'})"/>
-  <a-descriptions>
-    <a-descriptions-item label="用户名">{{user.username}}</a-descriptions-item>
-    <a-descriptions-item label="UID">{{user.uid}}</a-descriptions-item>
-    <a-descriptions-item label="权限">{{user.role}}</a-descriptions-item>
-  </a-descriptions>
-  <a-button @click="logout">登出</a-button>
-  <a-row v-if="user.role === 'super-admin'">
-    <a-input-group compact>
-      <a-input v-model:value="phone" style="width: 120px" />
-      <a-button type="primary" @click="addAdmin">添加管理员</a-button>
-    </a-input-group>
-  </a-row>
+  <div style="margin: 20px 50px; display: flex">
+    <a-descriptions :column="1" style="width: 20em">
+      <a-descriptions-item label="用户名">{{user.username}}</a-descriptions-item>
+      <a-descriptions-item label="UID">{{user.uid}}</a-descriptions-item>
+      <a-descriptions-item label="权限">{{userRole}}</a-descriptions-item>
+    </a-descriptions>
+    <div>
+      <a-button @click="logout">登出</a-button>
+      <a-row v-if="user.role === 'super-admin'" style="margin-top: 40px">
+        <a-input-group compact>
+          <a-input v-model:value="phone" style="width: 120px" />
+          <a-button type="primary" @click="addAdmin">添加管理员</a-button>
+        </a-input-group>
+      </a-row>
+    </div>
+  </div>
 </template>
 
 <style scoped>
