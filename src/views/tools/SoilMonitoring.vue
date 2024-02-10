87d1d105b7e1e5878a27494a01dc12e7c99f910c
Shxuuer <a2711492759@163.com>
2024-02-10 16:47:37 +0800

    fix(login): 修复已知bug，优化登陆体验

--- src/views/tools/SoilMonitoring.vue
+++ src/views/tools/SoilMonitoring.vue
@@ -12,15 +12,16 @@ export default {
       time: null,
       data: [],
       loading: false,
-      instrList: []
+      instrList: [],
+      timeSelected: false
     }
   },
   computed: {
     convertToJson () {
       return {
         sensor_id: this.selectId,
-        start_time: dayjs(this.time[0].$d).format('YYYY-MM-DDTHH:mm:ssZ'),
-        end_time: dayjs(this.time[1].$d).format('YYYY-MM-DDTHH:mm:ssZ')
+        start_time: this.timeSelected ? dayjs(this.time[0].$d).format('YYYY-MM-DDTHH:mm:ssZ') : undefined,
+        end_time: this.timeSelected ? dayjs(this.time[1].$d).format('YYYY-MM-DDTHH:mm:ssZ') : undefined
       }
     }
   },
@@ -31,8 +32,8 @@ export default {
         message.warn('请选择机器号')
         return
       }
-      if (this.time === null) {
-        message.warn('请选择时间线')
+      if (this.timeSelected && this.time === null) {
+        message.warn('请选择时间')
         return
       }
       this.loading = true
@@ -62,44 +63,55 @@ export default {
 
 <template>
   <a-page-header style="border-bottom: 1px solid rgb(235, 237, 240);padding: 10px 20px;font-size: 1.1em" title="土壤数据监控" @back="$router.push({name: 'Home'})"/>
-  <div>
-    <a-row style="width: 90%;margin: 20px auto">
-      <a-dropdown>
-        <a @click.prevent style="font-size: 1.2em;margin-right: 30px;" >
-          {{ selectId === 0 ? '选择仪器编号' : `已选中${selectId}号检测器` }}
-          <DownOutlined />
-        </a>
-        <template #overlay>
-          <a-menu>
-            <a-menu-item v-for="(item, index) in instrList" :key="index" @click="selectId = item.id">
+  <div style="width: 100%">
+    <a-row class="filter">
+      <a-row>
+        <a-dropdown>
+          <a @click.prevent style="font-size: 1.2em;margin-right: 30px;" >
+            {{ selectId === 0 ? '选择仪器编号' : `已选中${selectId}号检测器` }}
+            <DownOutlined />
+          </a>
+          <template #overlay>
+            <a-menu>
+              <a-menu-item v-for="(item, index) in instrList" :key="index" @click="selectId = item.id">
               <span>
                 {{item.id}}号 {{item.location}} {{item.function}}
                 <span v-if="item.status === '故障'" style="color: red">（故障）</span>
               </span>
-            </a-menu-item>
-          </a-menu>
-        </template>
-      </a-dropdown>
-      <a-range-picker v-model:value="time" show-time style="margin-right: 10px"/>
-      <a-button type="primary" @click="fetchData">查询</a-button>
+              </a-menu-item>
+            </a-menu>
+          </template>
+        </a-dropdown>
+      </a-row>
+      <a-row>
+        <a-checkbox style="margin-right: 10px" v-model:checked="timeSelected"/>
+        <a-range-picker v-model:value="time" show-time style="margin-right: 10px" :disabled="!timeSelected"/>
+      </a-row>
+      <a-row>
+        <a-button type="primary" @click="fetchData">查询</a-button>
+      </a-row>
     </a-row>
 
-    <a-row style="margin-top: 20px;">
+    <div style="margin-top: 20px;width: 100%">
       <a-row style="width: 100%">
         <a-spin :spinning="loading" style="margin: auto;"/>
       </a-row>
       <a-row v-if="!loading" style="width: 100%">
-        <a-descriptions :title="`${item.sensor_id}号土壤传感器`" bordered class="data" v-for="(item, index) in data" :key="index">
-          <a-descriptions-item label="传感器编号">{{item.sensor_id}}</a-descriptions-item>
-          <a-descriptions-item label="传感器信息" :span="2">{{item.sensor_info}}</a-descriptions-item>
-          <a-descriptions-item label="电导率">{{item.sensor_data.conductivity}}</a-descriptions-item>
-          <a-descriptions-item label="温度">{{item.sensor_data.temperature}}</a-descriptions-item>
-          <a-descriptions-item label="湿度">{{item.sensor_data.humidity}}</a-descriptions-item>
-          <a-descriptions-item label="PH值">{{item.sensor_data.ph}}</a-descriptions-item>
-          <a-descriptions-item label="检测时间">{{dayjs(item.sensor_data.detect_time).format('YYYY-MM-DD HH:mm:ss')}}</a-descriptions-item>
-        </a-descriptions>
+        <a-row style="width: 90%;margin: 10px auto;" v-if="data.length !== 0">传感器信息：{{data[0].sensor_info}}</a-row>
+        <a-row style="width: 100%">
+          <a-descriptions bordered class="data" v-for="(item, index) in data" :key="index">
+            <a-descriptions-item label="检测时间" :span="2">{{dayjs(item.sensor_data.detect_time).format('YYYY-MM-DD HH:mm:ss')}}</a-descriptions-item>
+            <a-descriptions-item label="温度(℃)">{{item.sensor_data.temperature}}</a-descriptions-item>
+            <a-descriptions-item label="电导率(us/cm)">{{item.sensor_data.conductivity}}</a-descriptions-item>
+            <a-descriptions-item label="湿度(%)">{{item.sensor_data.humidity}}</a-descriptions-item>
+            <a-descriptions-item label="PH值(pH)">{{item.sensor_data.ph}}</a-descriptions-item>
+            <a-descriptions-item label="氮指数(mg/kg)">{{item.sensor_data.nitrogen}}</a-descriptions-item>
+            <a-descriptions-item label="磷指数(mg/kg)">{{item.sensor_data.phosphorus}}</a-descriptions-item>
+            <a-descriptions-item label="钾指数(mg/kg)">{{item.sensor_data.potassium}}</a-descriptions-item>
+          </a-descriptions>
+        </a-row>
       </a-row>
-    </a-row>
+    </div>
   </div>
 </template>
 
@@ -113,5 +125,16 @@ export default {
   margin: 10px auto;
   border-top: 1px solid #a3a3a3;
   padding-top: 20px;
+}
+
+.filter {
+  width: 90%;
+  margin: 20px auto;
+  display: flex;
+  flex-direction: column
+}
+
+.filter:deep(.ant-row) {
+  margin-bottom: 15px;
 }
 </style>
